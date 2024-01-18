7741f7bdc04b8e06e9e43c210bb7ec80e5a40523
dganicolas <dganicolas@iesrafaelalberti.es>
2024-01-18 21:30:00 +0100

    AWERWGe567eu6w4t5uw45u

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,26 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+class Persona(var peso:Double,var altura:Double){
+    var nombre = ""
+    var imc= 0.0
+    constructor(nombre:String, peso:Double, altura:Double):this(peso, altura){
+    this.nombre=nombre
+    }
+}
+fun main() {
+    /*
+    * --Crear una clase Persona que tenga nombre, peso (en kg con decimales), altura (en metros con decimales) y su imc.
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+--Crear un constructor primario con todos los atributos, excepto nombre e imc. Este último atributo se calcula en función del peso y la altura. Por tanto no se debe poder modificar, pero si consultar.
+
+--Crear un constructor secundario que también incluya el nombre de la persona cómo parámetro.
+
+Implementa el método toString, representación del objeto en forma de String: override fun toString() = "". (Pulsa Ctrl+o)
+
+En el main() a) crear 3 personas diferentes (la primera sin nombre) utilizando el constructor primario y secundario y b) mostrarlas por consola. A continuación, realizar lo siguiente:
+
+Sobre la persona 1: a) modificar su nombre y para ello debes solicitarlo al usuario por consola. No puede ser nulo o vacio. b) Mostrar por consola sólo el nombre, peso y altura.
+Sobre la persona 3: a) Mostrar el peso, altura y imc. b) Modificar la altura, por ejemplo a 1.80 c) Mostrar el peso, altura y imc.
+Sobre la persona 2; a) Modificar la altura para que tenga el mismo valor que la persona 3. b) Mostrar la persona 2 y persona 3. c) Comparar si las dos personas son iguales, y mostrar el resultado. d) Implementa el método equals():boolean (Pulsa Ctrl+o). e) Ejecutar la comparación.
+    * */
+    val persona1 = Persona(23.0,23.0)
+    val persona2 = Persona("AAAAAAAAAAA",23.0,23.0)
 }
